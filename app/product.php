886e18b291cafa4ca85d24d8249ce62f286b0bee
西本展樹 <nishimotohiroki@nishihontenjunoMacBook-Pro.local>
2022-01-30 10:47:35 +0900

    10 create favorite

--- app/product.php
+++ app/product.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+ use Overtrue\LaravelFavorite\Traits\Favoriteable;
 
 class product extends Model
 {
+    use Favoriteable;
+
     public function category()
     {
         return $this->belongsTO('App\Category');
